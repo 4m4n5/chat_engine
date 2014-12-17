d82a52f41093859d6dfb761fbcf2dc4ddabad936
Aman Shrivastava <aman.srivastava999@gmail.com>
2014-12-18 03:27:23 +0530

    Added MongoLab DB

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var mongo = require('mongodb').MongoClient,
     client = require('socket.io').listen(8080).sockets;
 
-mongo.connect('mongodb://127.0.0.1/chat',function(err, db){
+mongo.connect('mongodb://aman:thermo999@ds063870.mongolab.com:63870/chat',function(err, db){
     if (err) throw err;
     
     client.on('connection', function(socket){
