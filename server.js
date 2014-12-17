6bb7561035a4a8efd8c970cfcf96ea5c773b649f
Aman Shrivastava <aman.srivastava999@gmail.com>
2014-12-17 20:11:59 +0530

    updated hostnames

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var mongo = require('mongodb').MongoClient,
     client = require('socket.io').listen(8080).sockets;
 
-mongo.connect('mongodb://127.0.0.1/chat',function(err, db){
+mongo.connect('mongodb://amanthedorkknight.github.io/chat_engine/chat',function(err, db){
     if (err) throw err;
     
     client.on('connection', function(socket){
